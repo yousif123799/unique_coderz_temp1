8c1ec51aad3c2cf5b978f90d79ca5ffb7d2aa86b
shebba <generlyousif55555shabasy@gmail.com>
2025-03-12 16:55:48 +0200

    create project

---
+++ script.js
@@ -0,0 +1,7 @@
+// // const x= open.window('https://www.google.com', 'width=600, height=400');
+// // console.log(x);
+
+// setTimeout(()=>console.log("hi"),5000)
+// console.log(navigator.href);
+// console.log(window.location.href);
+// setInterval(()=>console.log("hi"),500);
